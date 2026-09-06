e8335e56341818d6fceeda828662da79955a0e47
Mustafa Adel <mustafa.adel@nweave.com>
2026-09-07 02:01:43 +0300

    work done ========= - read body to read body of post requests. - read json to parse body for unkown. - send json help to pass message in the client,

---
+++ src/http.ts
@@ -0,0 +1,29 @@
+import {IncomingMessage , ServerResponse} from 'node:http';
+
+// send Json - read Json - read Body
+
+export function sendJson(res:ServerResponse,status:number,body:unknown):void{
+    const json = JSON.stringify(body);
+    res.writeHead(status,{
+        "content-type":"application/json; charset=utf-8",
+        "content-length": Buffer.byteLength(json)
+    });
+    res.end(json);
+}
+
+export function readBody(req:IncomingMessage): Promise<string>{
+
+    return new Promise((resolve,reject) => {
+        const chunks:Buffer[] = [];
+        req.on("data",(chunk:Buffer) => chunks.push(chunk));
+        req.on("end",() => Buffer.concat(chunks).toString("utf-8"));
+        req.on("error",reject);
+    });
+}
+
+export async function readJson(req: IncomingMessage): Promise<unknown>{
+
+    const body = readBody(req);
+    if(!body) return {};
+    return JSON.stringify(body) as unknown;
+} // why return unknown ? does it our best option ? could it cause issue if the client return something exactly ?
