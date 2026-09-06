679bd449f11b6582176d458b7e0f4dab051b8ceb
Mustafa Adel <mustafa.adel@nweave.com>
2026-09-07 01:58:14 +0300

    work done: ========== - create types for link service.

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type Link = {
+    code: string;
+    url: string;
+    createdAt: string;
+    clicks: number;
+}
+
+export type CreateLinkInput = {
+    url: string;
+}
+
+export type Result<D, E = string> = 
+    | {ok:true;data:D} 
+    | {ok:false;error:E}
